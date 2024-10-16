6065e7b3cf184a9ed5e19d1463eec83f910be8f6
Ananya Srivastav <59044922+ananyasrivastav@users.noreply.github.com>
2024-10-16 23:22:15 +0530

    c

---
+++ product/src/main/java/com/product/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.product.product.repository;
+
+import com.product.product.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+}
