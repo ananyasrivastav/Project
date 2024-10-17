01199dfab8d35be2d6bf754d75903869c0614922
Ananya Srivastav <59044922+ananyasrivastav@users.noreply.github.com>
2024-10-17 17:34:27 +0530

    mongo db changes and Test Classes added

--- product/src/test/java/com/product/product/ProductApplicationTests.java
+++ product/src/test/java/com/product/product/ProductApplicationTests.java
@@ -1,15 +1,54 @@
 package com.product.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.util.regex.Matcher;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
 	@Test
 	void contextLoads() {
+		String requestBody = """
+				{
+				    "name":"Iphone16",
+				    "description":"ghatiya phone",
+				    "price": 1000
+				}
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name",Matchers.equalTo("Iphone16"));
 	}
 
 }
